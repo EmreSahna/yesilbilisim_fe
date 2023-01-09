282d225d5a18db4a2a4ba0277f99db84a2b40133
EmreSahna <emre.sahna@hotmail.com>
2023-01-09 21:27:44 +0300

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 import Topnavbar from "./common/topnavbar";
 import Navbar from "./common/navbar";
 import Spinner from "./common/spinner";
 import Footer from "./common/footer";
 import Copyright from "./common/copyright";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -20,11 +21,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hakkimizda" element={<About />} />
         <Route path="/iletisim" element={<Contact/>} />
-        {/* 
         <Route path="*" element={<NotFound />} /> 
+        {/* 
         <Route path="urunler" element={<Products />} />
         <Route path="hizmetler" element={<Services />} />
         <Route path="blog" element={<Blog />} />
+        <Route path="admin" element={<Admin />} />
         */}
       </Routes>
       <Footer/>
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+    return (
+        <div className="bg-main-blue flex justify-end h-screen items-center">
+            <img src="http://localhost:8080/img/logo/logo_3.png" className="absolute left-[190px] items-center w-[300px] " alt="404" />
+            <div className="text-white w-1/2 z-10 text-left mr-16">
+                <p className="text-[36px] font-bold">Sayfa Bulunamadı !</p>
+                <p className="text-md font-semibold">Üzgünüz, ancak belirtilen sayfa bulunamadı. Bu hatanın nedeni, adresin geçersiz olması veya sayfanın internette artık mevcut olmaması olabilir. Eğer sorununuz devam ediyorsa, lütfen site yöneticisiyle iletişime geçiniz.</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
