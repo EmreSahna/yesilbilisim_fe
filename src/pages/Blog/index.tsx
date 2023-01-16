94859f0ea4fd65e6e91501448050159dd0c9b595
EmreSahna <emre.sahna@hotmail.com>
2023-01-16 19:04:36 +0300

    blog page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -10,6 +10,86 @@ const Blog = () => {
                 </ul>
                 <p className="text-white font-bold text-[28px]">Blog</p>
             </div>
+            <div className="flex justify-center items-center my-8 gap-5 flex-wrap">
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-[320px] h-fit p-[20px] bg-main-gray rounded-lg shadow-product">
+                    <div className="relative h-[240px] py-[20px] bg-white rounded-md">
+                        <img src="medium_1.webp" className="w-full" alt="" />
+                    </div>
+                    <h3 className="font-bold mt-[10px] text-[22px] text-black">Java Reflection Nedir ?</h3>
+                    <p className="font-semibold text-black mt-[5px] text-justify">Reflection; classları, metodları, interfaceleri, fieldları, vb. özellikleri programı...</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="flex items-center bg-main-blue text-white py-1 px-2 rounded-lg" type="button">
+                            <span className="text-[12px] font-semibold text-white">Devamını Oku</span>
+                            <i className="font-arrow not-italic rotate-90 inline-block text-main-black text-[12px] ml-2 mt-0.5">¥</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
