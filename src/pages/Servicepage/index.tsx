b56eef8555bf2d0da7420ee0fae8bf5845253da8
Emre Şahna <emre.sahna@hotmail.com>
2023-02-20 17:19:56 +0300

    changes

--- src/pages/Servicepage/index.tsx
+++ src/pages/Servicepage/index.tsx
@@ -13,11 +13,36 @@ const Servicepage = () => {
             document.title = res.data.title +" | Yeşil Bilişim";
         });
     }, [id]);
+
+    useEffect(() => {
+        if (!service.cardContent) {
+            return;
+        }
+        let parser = new DOMParser();
+        let htmlDoc = parser.parseFromString(service.cardContent, "text/html");
+        let blog = document.getElementById("blog-content");
+        let elements = Array.from(htmlDoc.body.children);
+        elements.forEach(element => {
+            blog?.appendChild(element);
+        });
+        
+    }, [service.cardContent])
     
     return (
-        <div>
-        <h1>{service.title}</h1>
-        </div>
+        <>
+            <div className="h-auto bg-main-blue flex justify-center py-6 flex-col items-center">
+                <img src="http://localhost:8080/img/logo/logo_2.png" className="h-[100px] mb-2" alt="" />
+                <ul className="my-4">
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                </ul>
+                <p className="text-white font-bold text-[28px]">{service.title}</p>
+            </div>
+            <div className="my-[50px] max-w-[1170px] mx-auto flex flex-col gap-[30px] text-base text-justify" id="blog-content">
+                <h1 className="text-[24px] text-main-blue font-semibold">{service.title}</h1>
+            </div>
+        </>
     );
 };
 
--- src/pages/Solutionpage/index.tsx
+++ src/pages/Solutionpage/index.tsx
@@ -14,16 +14,35 @@ const Solutionpage = () => {
         });
     }, [id]);
 
+    useEffect(() => {
+        if (!solution.cardContent) {
+            return;
+        }
+        let parser = new DOMParser();
+        let htmlDoc = parser.parseFromString(solution.cardContent, "text/html");
+        let blog = document.getElementById("blog-content");
+        let elements = Array.from(htmlDoc.body.children);
+        elements.forEach(element => {
+            blog?.appendChild(element);
+        });
+        
+    }, [solution.cardContent])
     
     return (
-        <div className="container">
-        <div className="row">
-            <div className="col-md-12">
-            <h1>{solution.title}</h1>
-            <p>{solution.cardContent}</p>
+        <>
+            <div className="h-auto bg-main-blue flex justify-center py-6 flex-col items-center">
+                <img src="http://localhost:8080/img/logo/logo_2.png" className="h-[100px] mb-2" alt="" />
+                <ul className="my-4">
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                </ul>
+                <p className="text-white font-bold text-[28px]">{solution.title}</p>
             </div>
-        </div>
-        </div>
+            <div className="my-[50px] max-w-[1170px] mx-auto flex flex-col gap-[30px] text-base text-justify" id="blog-content">
+                <h1 className="text-[24px] text-main-blue font-semibold">{solution.title}</h1>
+            </div>
+        </>
     );
 };
 
