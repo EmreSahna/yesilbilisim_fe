6dbcab438125cf79a6fc2029d74b1bef38769f52
Emre Şahna <emre.sahna@hotmail.com>
2023-02-20 17:58:47 +0300

    blog title

--- src/pages/Blogpage/index.tsx
+++ src/pages/Blogpage/index.tsx
@@ -39,17 +39,27 @@ const Blogpage = () => {
     }, [post.blogContent])
 
     return (
-        <div className="max-w-[1170px] mx-auto flex my-[50px]">
-            <div className="w-[75%] flex flex-col gap-8" id="blog-content">
-                <h1 className="text-[36px] font-bold text-center text-main-black">{post.title}</h1>
-                <div className="flex items-center text-main-blue gap-[2px]">
-                    <p className="material-icons-round text-[16px]">schedule</p>
-                    <h2 className="text-[14px] font-semibold">{post.createdDate}</h2>
-                </div>
-                <p className="text-[22px] font-noto">{post.description}</p>
+        <>
+            <div className="h-auto bg-main-blue flex justify-center py-6 flex-col items-center">
+                <img src="http://localhost:8080/img/logo/logo_2.png" className="h-[100px] mb-2" alt="" />
+                <ul className="my-4">
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                    <li className="h-[16px] w-[16px] bg-white rounded-full mx-2 inline-block" />
+                </ul>
+                <p className="text-white font-bold text-[28px]">{post.title}</p>
             </div>
-            <BlogNavigate />
-        </div>
+            <div className="max-w-[1170px] mx-auto flex my-[50px]">
+                <div className="w-[75%] flex flex-col gap-8" id="blog-content">
+                    <div className="flex items-center text-main-blue gap-[2px]">
+                        <p className="material-icons-round text-[16px]">schedule</p>
+                        <h2 className="text-[14px] font-semibold">{post.createdDate}</h2>
+                    </div>
+                    <p className="text-[22px] font-noto">{post.description}</p>
+                </div>
+                <BlogNavigate />
+            </div>
+        </>
     )
 }
 
